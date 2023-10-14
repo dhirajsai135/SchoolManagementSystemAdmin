2e8e297ec50d190d5eae2f1c12c85b8f69be0b09
Gandrakota Dhiraj venkata phani saikrishna <82799252+dhirajsai135@users.noreply.github.com>
2023-10-14 22:41:40 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SchoolManagementSystemAdmin.Models;
+using System.Text.Json;
+
+namespace SchoolManagementSystemAdmin.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+    public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient("BaseUrl");
+        var response = await client.GetAsync("api/Students/GetAll");
+        var result = response.Content.ReadAsStringAsync();
+        var example = JsonConvert.DeserializeObject<List<StudentVM>>(result.Result);
+        return View(example);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
---
+++ Models/StudentVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace SchoolManagementSystemAdmin.Models;
+
+public class StudentVM
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string? Gender { get; set; }
+    public string? Address { get; set; }
+    public int ClassId { get; set; }
+}
